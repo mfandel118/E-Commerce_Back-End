f26284730568c53f35fa23fbe82259831d05cb1a
Marissa Fandel <mfandel@alumni.nd.edu>
2022-04-03 14:10:47 -0600

    finsihed categories GET routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,6 +3,7 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
+// Get All Categories
 router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
@@ -10,17 +11,35 @@ router.get('/', async (req, res) => {
     const categories = await Category.findAll({ 
       include: [{ model: Product }] 
     });
+
     res.status(200).json(categories);
+
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
+// Get 1 specific category
 router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const category = await Category.findByPk(req.params.id, { 
+      include: [{ model: Product }] 
+    });
 
+    if ((!category)) {
+      res.status(404).json({ message: "No category found with that ID."});
+      return;
+    }
+
+    res.status(200).json(category);
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+
 
 router.post('/', async (req, res) => {
   // create a new category
